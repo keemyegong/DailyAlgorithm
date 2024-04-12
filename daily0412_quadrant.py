bfdb97ba40603e511b7fd8463d1af2beaeb36baa
keemyegong <fridaycomeslow@gmail.com>
2024-04-12 20:28:18 +0900

    daily0412_quadrant

---
+++ daily0412_quadrant.py
@@ -0,0 +1,13 @@
+# [BAEKJOON] 14681. 사분면 고르기
+
+x = int(input())
+y = int(input())
+
+if x > 0 and y > 0:
+  print(1)
+elif x < 0 and y > 0:
+  print(2)
+elif x < 0 and y < 0:
+  print(3)
+else:
+  print(4)
